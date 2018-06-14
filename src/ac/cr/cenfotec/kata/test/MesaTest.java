af64b6566fbe3745709831b49846eb4cbb111116
enzocr <equartinoz@ucenfotec.ac.cr>
2018-06-14 11:55:15 -0600

    Punto 6: Clase Mesa creada con repartidor y lista de jugadores, inicializada en CERO jugadores

---
+++ src/ac/cr/cenfotec/kata/test/MesaTest.java
@@ -0,0 +1,38 @@
+package ac.cr.cenfotec.kata.test;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+
+import ac.cr.cenfotec.logica.Jugador;
+import ac.cr.cenfotec.logica.Mesa;
+import ac.cr.cenfotec.logica.Naipe;
+import ac.cr.cenfotec.logica.Repartidor;
+
+public class MesaTest {
+
+	static Naipe naipe;
+	static Repartidor repartidor;
+	static Mesa mesa;
+	static ArrayList<Jugador> listaJugadores;
+	static Jugador jugador1;
+	static Jugador jugador2;
+	static Jugador jugador3;
+
+	@Before
+	public void init() throws Exception {
+		naipe = new Naipe();
+		repartidor = new Repartidor(naipe);
+		listaJugadores = new ArrayList<>();
+		mesa = new Mesa(repartidor);
+
+		Jugador jugador1 = new Jugador("Enzo");
+		Jugador jugador2 = new Jugador("Alonso");
+		Jugador jugador3 = new Jugador("Eduardo");
+
+		mesa.addJugador(jugador1);
+		mesa.addJugador(jugador2);
+		mesa.addJugador(jugador3);
+
+	}
+}
---
+++ src/ac/cr/cenfotec/logica/Mesa.java
@@ -0,0 +1,53 @@
+package ac.cr.cenfotec.logica;
+
+import java.util.ArrayList;
+
+public class Mesa {
+
+	private Repartidor repartidor;
+	private ArrayList<Jugador> jugadores;
+
+	public Mesa() {
+		this.repartidor = null;
+		this.jugadores = new ArrayList<>();
+	}
+
+	public Mesa(Repartidor repartidor) {
+		this.repartidor = repartidor;
+
+	}
+
+	public Repartidor getRepartidor() {
+		return repartidor;
+	}
+
+	public void setRepartidor(Repartidor repartidor) {
+		this.repartidor = repartidor;
+	}
+
+	public ArrayList<Jugador> getJugadores() {
+		return jugadores;
+	}
+
+	public void setJugadores(ArrayList<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}
+
+	public boolean addJugador(Jugador jugador) throws Exception {
+		if (jugadoresIsNull()) {
+
+			getJugadores().add(jugador);
+
+		}
+		return false;
+
+	}
+
+	public boolean jugadoresIsNull() {
+		if (this.jugadores == null) {
+			this.jugadores = new ArrayList<>();
+			return true;
+		}
+		return true;
+	}
+}
