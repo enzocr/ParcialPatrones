f48bb2da532607e8c6b169d52e4471f9b05dc9e0
enzocr <equartinoz@ucenfotec.ac.cr>
2018-06-14 11:34:26 -0600

    Punto 2: Metodo equals sobreescrito en la clase Carta junto con su prueba respectiva en CartaTest. Pruebas refactorizadas en CartaTest

--- src/ac/cr/cenfotec/kata/test/CartaTest.java
+++ src/ac/cr/cenfotec/kata/test/CartaTest.java
@@ -1,26 +1,51 @@
 package ac.cr.cenfotec.kata.test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import ac.cr.cenfotec.logica.Carta;
 
 public class CartaTest {
 
-	static Carta carta = new Carta();
+	static Carta carta;
+	static Carta uno;
+	static Carta dos;
+	static Carta tres;
+
+	@Before
+	public void init() throws Exception {
+		carta = new Carta();
+		uno = new Carta("Diez", "Escudos", 10);
+		dos = new Carta("Diez", "Flores", 10);
+		tres = new Carta("Ocho", "Flores", 8);
+	}
 
 	@Test(expected = Exception.class)
 	public void menorQueCero() throws Exception {
-		new Carta("Tres", "Estrellas", -80);
+		Carta prueba = new Carta("Tres", "Estrellas", -80);
+		assertFalse(uno.equals(prueba));
 	}
 
 	@Test(expected = Exception.class)
 	public void mayorQueDoce() throws Exception {
-		new Carta("Tres", "Estrellas", 80);
+		Carta prueba = new Carta("Tres", "Estrellas", 80);
+		assertFalse(dos.equals(prueba));
 	}
 
 	@Test
 	public void entreCeroYDoce() throws Exception {
-		new Carta("Tres", "Estrellas", 3);
+		Carta prueba = new Carta("Tres", "Estrellas", 3);
+		assertFalse(tres.equals(prueba));
+	}
+
+	@Test
+	public void cartasIguales() throws Exception {
+
+		assertTrue(uno.equals(dos));
+		assertFalse(dos.equals(tres));
 	}
 
 }
--- src/ac/cr/cenfotec/logica/Carta.java
+++ src/ac/cr/cenfotec/logica/Carta.java
@@ -60,4 +60,31 @@ public class Carta {
 	public String toString() {
 		return "Carta [nombre=" + nombre + ", palo=" + palo + ", valor=" + valor + "]\n";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + valor;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Carta other = (Carta) obj;
+		if (valor != other.valor) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
